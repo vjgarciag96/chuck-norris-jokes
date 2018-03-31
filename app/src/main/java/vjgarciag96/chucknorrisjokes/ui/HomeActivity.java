89902b813a26451ae62e56d934d413c7211cb456
vjgarciag96 <vjgarciag96@gmail.com>
2018-03-31 16:55:15 +0200

    Add progress bar for displaying during joke fetching

--- app/src/main/java/vjgarciag96/chucknorrisjokes/ui/HomeActivity.java
+++ app/src/main/java/vjgarciag96/chucknorrisjokes/ui/HomeActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +25,7 @@ public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "Home";
 
     @BindView(R.id.joke_content) TextView jokeContentTextView;
+    @BindView(R.id.progress_bar) ProgressBar progressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,15 +35,19 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.chuck_face)
-    public void onNewJokeButtonClicked(){
+    public void onNewJokeButtonClicked() {
+        cleanJoke();
+        showProgressBar();
         APIManager.getRandomJokeService().getRandomJoke().enqueue(new Callback<APIResponse>() {
             @Override
             public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
                 Log.d(TAG, "Response code = " + String.valueOf(response.code()));
-                if(response != null && response.isSuccessful() &&
+
+                hideProgressBar();
+                if (response != null && response.isSuccessful() &&
                         response.body() != null && response.body().getValue() != null
                         && !TextUtils.isEmpty(response.body().getValue()))
-                    jokeContentTextView.setText(response.body().getValue());
+                    showJoke(response.body().getValue());
                 else
                     showToast(getString(R.string.network_error));
             }
@@ -48,16 +55,53 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<APIResponse> call, Throwable t) {
                 Log.d(TAG, "Error on API request = " + t.getMessage());
+                hideProgressBar();
                 showToast(getString(R.string.network_error));
             }
         });
     }
 
-    private void showToast(final String message){
+    private void showToast(final String message) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void showProgressBar() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void hideProgressBar() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void showJoke(final String jokeContent){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                jokeContentTextView.setText(jokeContent);
+            }
+        });
+    }
+
+    private void cleanJoke(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                jokeContentTextView.setText("");
             }
         });
     }
