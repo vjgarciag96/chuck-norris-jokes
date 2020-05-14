321529991ba44588eba118a98936debf5df74fe3
Víctor J. García <vjgarciaw@gmail.com>
2020-05-14 13:41:15 +0200

    Tests for jokes repository

---
+++ app/src/test/java/com/vjgarcia/chucknorrisjokes/data/JokesRepositoryTest.kt
@@ -0,0 +1,69 @@
+package com.vjgarcia.chucknorrisjokes.data
+
+import com.vjgarcia.chucknorrisjokes.RxUnitTest
+import com.vjgarcia.chucknorrisjokes.mappers.toJoke
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.Single
+import org.junit.jupiter.api.Test
+
+class JokesRepositoryTest : RxUnitTest() {
+
+    private val chuckNorrisApiServiceMock = mockk<ChuckNorrisApiService>()
+    private val chuckNorrisStorageMock = mockk<ChuckNorrisStorage>(relaxed = true)
+    private val categorySelectorMock = mockk<CategorySelector>()
+
+    private val sut = JokesRepository(
+        chuckNorrisApiServiceMock,
+        chuckNorrisStorageMock,
+        categorySelectorMock
+    )
+
+    @Test
+    fun `categories successfully fetched from API are stored`() {
+        val anyCategories = listOf("a", "b", "c")
+        every { chuckNorrisApiServiceMock.categories() } returns Single.just(anyCategories)
+        every { categorySelectorMock.select(anyCategories) }.returnsMany("b", "c")
+        every { chuckNorrisApiServiceMock.randomByCategory(any()) }.returnsMany(
+            Single.just(JokeDto(id = "1", text = "hey", categories = listOf("b"))),
+            Single.just(JokeDto(id = "2", text = "que pacha bro", categories = listOf("c")))
+        )
+
+        sut.randomStream(2).test().dispose()
+
+        verify { chuckNorrisStorageMock.categories = anyCategories }
+    }
+
+    @Test
+    fun `categories from storage are returned when fetch from API fails`() {
+        val anyCategories = listOf("a", "b", "c")
+        every { chuckNorrisApiServiceMock.categories() } returns Single.error(Exception())
+        every { chuckNorrisStorageMock.categories } returns anyCategories
+        every { categorySelectorMock.select(anyCategories) }.returnsMany("b", "c")
+        every { chuckNorrisApiServiceMock.randomByCategory(any()) }.returnsMany(
+            Single.just(JokeDto(id = "1", text = "hey", categories = listOf("b"))),
+            Single.just(JokeDto(id = "2", text = "que pacha bro", categories = listOf("c")))
+        )
+
+        sut.randomStream(2).test().dispose()
+
+        verify { chuckNorrisStorageMock.categories }
+    }
+
+    @Test
+    fun `correspondent jokes by category are returned`() {
+        val anyCategories = listOf("a", "b", "c")
+        val anyBJoke = JokeDto(id = "1", text = "hey", categories = listOf("b"))
+        val anyCJoke = JokeDto(id = "2", text = "que pacha bro", categories = listOf("c"))
+        every { chuckNorrisApiServiceMock.categories() } returns Single.error(Exception())
+        every { chuckNorrisStorageMock.categories } returns anyCategories
+        every { categorySelectorMock.select(anyCategories) }.returnsMany("b", "c")
+        every { chuckNorrisApiServiceMock.randomByCategory("b") } returns Single.just(anyBJoke)
+        every { chuckNorrisApiServiceMock.randomByCategory("c") } returns Single.just(anyCJoke)
+
+        val jokesStream = sut.randomStream(2).test()
+
+        jokesStream.assertValues(anyBJoke.toJoke(), anyCJoke.toJoke()).dispose()
+    }
+}
